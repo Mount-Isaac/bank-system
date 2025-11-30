6bee7366c49985a69838276ffd01e3f548c99ca4
Isaac Kyalo <=>
2025-11-30 05:12:42 +0300

    added authMiddleware: check & verify Bearer token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,17 +1,27 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from 'jsonwebtoken'
+const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'testing-secret-key';
 
-const authMiddleware = (req:Request, res:Response, next:NextFunction) => {
-    const token =  req.headers.authorization?.split(" ")[1]
-    if(!token){
-        return res.status(401).json({ message: "No token provided"})
-    }
-    try{
-        req.user = jwt.verify(token, JWT_SECRET);
-        next();
-    }catch (error){
-        res.status(401).json({ message: 'Invalid token'})
-    }
+interface AuthRequest extends Request {
+  user?: any;
 }
+
+export const authMiddleware = (
+  req: AuthRequest, 
+  res: Response, 
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+  
+  try {
+    req.user = jwt.verify(token, JWT_SECRET);
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { allUsers, singleUser } from "../controllers/userController";
 import { validateUserId } from "../middleware/validation";
+import { authMiddleware } from "../middleware/auth";
 
 const router = express.Router()
 
 
-router.post("/", allUsers)
-router.get("/:id", validateUserId, singleUser)
-router.put("/:id", validateUserId, allUsers)
-router.delete("/:id", validateUserId, allUsers)
+router.post("/", authMiddleware, allUsers)
+router.get("/:id", authMiddleware, validateUserId, singleUser)
+router.put("/:id", authMiddleware, validateUserId, allUsers)
+router.delete("/:id", authMiddleware, validateUserId, allUsers)
 
 export default router
